5e6fa2fad134f92097fb6d5aeb6fd423b922c265
Joseph Khawly <joseph.khawly@gmail.com>
2024-07-01 22:17:30 -0400

    color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'dark-magenta': '#601E1E',
+      'light-pink': '#F4D3D3',
+      'burnt-orange': '#D74712',
+      'dusty-rose': '#E46E6D',
+      'bubblegum-pink': '# FF6489',
+      'barbie-pink': '#FF99B6',
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
