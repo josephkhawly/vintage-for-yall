a7073e46c81c86c587bd8dc581e02bca292857a8
Joseph Khawly <joseph.khawly@gmail.com>
2024-07-03 22:52:56 -0400

    final article upload

---
+++ app/portfolio/moms-80s-clothes/page.tsx
@@ -0,0 +1,156 @@
+import Image from "next/image";
+
+export default function Mom() {
+  return (
+    <article>
+      <h1 className='text-5xl mb-[15px]'>
+        My Mom&rsquo;s &lsquo;80s NYC Style Inspired My Favorite Winter Staples
+      </h1>
+      <h2 className='font-bold text-xl mb-[30px]'>
+        Despite my teen years blogging on Tumblr, I’ll never be as cool
+      </h2>
+      <p className='mb-5'>
+        Buying vintage and secondhand via Etsy or at your local Goodwill often signifies an
+        eco-conscious clothing fan or affiliation with the notorious title of &ldquo;Depop
+        Girlie.&rdquo; &nbsp;However, in my mother&rsquo;s teen years, vintage clothes were worn to
+        express your identity as a part of a subculture.
+      </p>
+      <p className='mb-5'>
+        New subcultures today are rare, with their roots stemming from niche and often outcast
+        communities that didn&rsquo;t prescribe to the social norm either as a political statement
+        or to simply find other people who equally bodied their interests. From punk to steampunk,
+        subculture has always existed alongside vintage, secondhand, and DIY clothing.
+      </p>
+      <p className='mb-5'>
+        My mother, then Marialena Morelli, grew up in the Bronx&rsquo;s Pelham Bay and spent her
+        free time thrifting and clubbing in the city, making her way to cultural landmarks of
+        &lsquo;80s NY like Area and The Peppermint Lounge, where she attended one of the live
+        recordings of Smell of Female by The Cramps.{' '}
+      </p>
+      <p className='mb-5'>
+        I asked Marialena how she would have defined herself back then. &ldquo;Categorically, I was
+        a new wave kid, but I had some good punk looks, too. We&rsquo;d get the Village Voice to see
+        which bands were playing and then check the ads section for any sales on vintage
+        clothes&rdquo; she laughs. Despite my teen years giving me a brief moment of fame via
+        Tumblr, I&rsquo;ll never be as cool.
+      </p>
+      <p className='mb-5'>
+        Her favorite stores? Canal Street Jeans and The Unique Boutique in The Village: a
+        half-vintage, half-modern one-stop shop for the best clubwear.
+      </p>
+      <p className='mb-5'>
+        Marialena reminisces on her favorite &lsquo;80s pieces you should add to your closet this
+        winter while I share the secondhand shopping tips to help you find them.
+      </p>
+      <p>
+        <Image alt='' src='/moms-80s-clothes/image4.png' width={353} height={529} />
+      </p>
+      <p>Marialena, pictured right, with her friend Lisa on their way to The Peppermint Lounge.</p>
+      <h2 className='font-bold text-3xl mt-5 mb-[15px] uppercase'>
+        MID-CENTURY BLACK WOOL OVERCOAT
+      </h2>
+      <p className='mb-5'>
+        Marialena acknowledges that this style coat was an attempt to get the London look, as punks
+        and new wavers across the pond utilized vintage menswear from the &lsquo;40s and &lsquo;50s
+        to get their point across. However, all-black outfits remain distinctly New York, yet that
+        monochromatic elegance can definitely travel.{' '}
+      </p>
+      <p className='mb-5'>
+        Sporting an understated black coat&nbsp;to turn an okay outfit into a conversation starter.
+        When searching eBay for mid-century coats, make sure you&rsquo;re looking into
+        &ldquo;overcoats,&rdquo; &ldquo;herringbone,&rdquo; and &ldquo;single-breasted.&rdquo;
+        However, if you want a silhouette that&rsquo;s distinctly &lsquo;80s with heavy shoulder
+        pads, use searches that include designers like Calvin Klein or features like double-breasted
+        and notched collars.
+      </p>
+      <p>
+        <Image alt='' src='/moms-80s-clothes/image1.png' width={353} height={529} />
+      </p>
+      <p>
+        <a href='https://www.therealreal.com/products/men/clothing/outerwear/pierre-cardin-vintage-1980-s-overcoat-k758t?sid=pxogmz&utm_source=google&utm_medium=shopping&utm_campaign=shopping_200&gad_source=1&gclid=Cj0KCQjw7ZO0BhDYARIsAFttkCjKNaMiWsUTV7TH0iBBx_rDqf28kpunPCMN5ehQDQKxwgFjHQmAn98aAu2gEALw_wcB'>
+          1980s Pierre Cardin Wool Overcoat
+        </a>
+        &nbsp;via The Real Real $245
+      </p>
+      <a id='t.6130f64f3ccef1c1f412bec246a7e62ed9da64cb'></a>
+      <a id='t.0'></a>
+      <h2 className='font-bold text-3xl mt-5 mb-[15px] uppercase'>TAPERED SUIT PANTS</h2>
+      <p className='mb-5'>
+        The best day-to-night item? Marialena says it&rsquo;s the high-waisted, tapered pant. What
+        makes these trousers great is their ability to give you a waist while still accomplishing
+        that effortless, baggy look.
+      </p>
+
+      <p className='mb-5'>
+        Mom suggests getting solid black or a color your closet is used to (she stuck with a deep,
+        plum purple). In addition to narrowing it down by color, I recommend playing around with
+        &ldquo;high waisted&rdquo; and &ldquo;pleated&rdquo; in your search. You&rsquo;ll want to
+        get these a little cropped or cuff them, so you can show off some colorful socks and a
+        ballet flat.
+      </p>
+      <p>
+        <Image alt='' src='/moms-80s-clothes/image3.png' width={353} height={529} />
+      </p>
+      <p>
+        <a href='https://www.etsy.com/listing/1739481512/small-80s-pleated-trousers-woven-high?ga_order=most_relevant&ga_search_type=vintage&ga_view_type=gallery&ga_search_query=80s+tapered+suit+pants&ref=sr_gallery-1-4&frs=1&cns=1&sts=1&content_source=e9cd3147ed8c0d5dfb6c488db410d5cb01a679ae%253A1739481512&organic_search_click=1'>
+          High Waisted Tapered Trousers{' '}
+        </a>
+        from BirdiesVintageCloset via Etsy&nbsp;$50
+      </p>
+
+      <h2 className='font-bold text-3xl mt-5 mb-[15px] uppercase'>
+        CARDIGAN WITH APPLIQUE OR BEADED DETAIL
+      </h2>
+      <p className='mb-5'>
+        Being big on borrowing from the family, Mom also loved sporting Nana&rsquo;s 1950s cardigan
+        that had the sweetest applique floral detail, but many styles from the time also featured
+        intricate beading. To better narrow things down, you can include terms like
+        &ldquo;rockabilly&rdquo; or swap &ldquo;50s&rdquo; for &ldquo;60s,&rdquo; as not every piece
+        will be from this decade (nor will every seller be great at dating their items). This soft
+        addition provides a fun contrast to a perfectly worn-in band tee and some combat boots but
+        can still read casual elegance if paired with tapered pants.{' '}
+      </p>
+
+      <p>
+        <Image alt='' src='/moms-80s-clothes/image5.png' width={353} height={529} />
+      </p>
+      <p>
+        <a href='https://www.etsy.com/listing/1685107292/vintage-50s-beaded-cardigan-sweater?ga_order=most_relevant&ga_search_type=vintage&ga_view_type=gallery&ga_search_query=applique+cardigan+50s&ref=sr_gallery-2-6&frs=1&content_source=e9bf228751789c95bb41626372244cd1009acd8a%253A1685107292&organic_search_click=1'>
+          Vintage 50s Lambswool Beaded Cardigan
+        </a>
+        &nbsp;from CharmVintageBotique via Etsy $68
+      </p>
+      <h2 className='font-bold text-3xl mt-5 mb-[15px] uppercase'>NEON SWEATER DRESSES</h2>
+      <p className='mb-5'>
+        Marialena loves those electric hues and hates that they&rsquo;ve turned into a clich&eacute;
+        often seen at Party City. After all, there is a right and a wrong way to wear neon. She
+        suggests finding a neon piece that&rsquo;s monochromatic or utilizes a chunkier knit and
+        paired with black accessories and silver hardware.{' '}
+      </p>
+
+      <p className='mb-5'>
+        &lsquo;80s sweaters, especially solids, can take a minute to locate online, but less is more
+        with your search. Stick as close to &ldquo;80s neon sweater dress&rdquo; as you can. Also,
+        don&rsquo;t dip into &lsquo;70s knits, as you&rsquo;ll see more of an A-line skirt versus
+        that straight, baggier style that looks closer to an elongated sweater versus an actual
+        dress. You can take this look out to dinner by opting for knee-high boots or take it to run
+        errands with sneakers and a lacey sock.
+      </p>
+
+      <p>
+        <Image
+          alt=''
+          src='/moms-80s-clothes/image2.png'
+          width={353}
+          height={529}
+        />
+      </p>
+      <p>
+        <a href='https://www.therealreal.com/products/women/clothing/dresses/betsey-johnson-vintage-knee-length-dress-ihzog'>
+          &lsquo;80s Betsey Johnson Sweater Dress
+        </a>
+        &nbsp;via TheRealReal $445
+      </p>
+    </article>
+  )
+}
