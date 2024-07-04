6076b4a1929b349df749081fa6bf182f2d09da87
Joseph Khawly <joseph.khawly@gmail.com>
2024-07-03 21:42:47 -0400

    press clippings

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,15 +1,33 @@
-import { sampleLinks } from '@/lib/writingSamples'
+import { pressClippings, sampleLinks } from '@/lib/writingSamples'
+import Image from 'next/image'
 
 export default function Portfolio() {
   return (
     <>
-      <h1 className='text-8xl mb-8'>Writing Samples</h1>
+      <h1 className='text-9xl mb-8'>Writing Samples</h1>
       {sampleLinks.map((sample) => (
         <div key={sample.slug} className='mb-4'>
           <a href={`/portfolio/${sample.slug}`}>{sample.title}</a>
         </div>
       ))}
-      <h1 className='text-8xl mb-8'>Published Press</h1>
+      <div className='flex justify-between'>
+        <h1 className='text-9xl mb-8'>Published Press</h1>
+        <div>
+          {pressClippings.map((clipping) => (
+            <div key={clipping.url} className='mb-4'>
+              <a href={clipping.url} target='_blank' rel='noopener noreferrer'>
+                <Image
+                  src={clipping.image}
+                  alt={clipping.alt}
+                  className='w-96'
+                  width={500}
+                  height={500}
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
--- lib/writingSamples.ts
+++ lib/writingSamples.ts
@@ -12,3 +12,16 @@ export const sampleLinks = [
     slug: 'ebay-skill-issue',
   },
 ]
+
+export const pressClippings = [
+  {
+    image: '/apartment-therapy.jpeg',
+    alt: 'Apartment Therapy',
+    url: 'https://www.apartmenttherapy.com/thrift-shopping-rewards-program-tip-37388679',
+  },
+  {
+    image: '/wwd.jpeg',
+    alt: 'WWD',
+    url: 'https://wwd.com/eye/parties/gallery/betsey-johnson-and-happy-hippie-pride-party-1235708048/betsey-johnson-and-happy-hippie-pride-party-14/',
+  },
+]
