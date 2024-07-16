5d0e67e62baa483a6b745a9bd01da6d80c5243df
Joseph Khawly <joseph.khawly@gmail.com>
2024-07-16 15:42:26 -0600

    publicity page subtitle style

--- app/publicity/page.tsx
+++ app/publicity/page.tsx
@@ -4,7 +4,7 @@ import { digitalPress } from '@/lib/publicity'
 export default function Portfolio() {
   return (
     <>
-      <h1>
+      <h1 className='text-center italic mb-7'>
         Click on the icons below to view highlights from various publicity work across entertainment
         and consumer clients
       </h1>
