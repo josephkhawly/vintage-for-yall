7b1d05b187dcf8c9b636284144f44c109ac102f8
Joseph Khawly <joseph.khawly@gmail.com>
2024-08-28 15:31:57 -0600

    feat: connect PLP and PDP to Shopify

---
+++ app/shop/[handle]/page.tsx
@@ -0,0 +1,97 @@
+import { HIDDEN_PRODUCT_TAG } from '@/lib/constants'
+import { getProduct } from '@/lib/shopify'
+import { Metadata } from 'next'
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
+
+interface PageProps {
+  params: { handle: string }
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const product = await getProduct(params.handle)
+
+  if (!product) return notFound()
+
+  const { url, width, height, altText: alt } = featuredImage || {}
+  const indexable = !product.tags.includes(HIDDEN_PRODUCT_TAG)
+
+  return {
+    title: `${product.seo.title || product.title} | Vintage for Y'all`,
+    description: product.seo.description || product.description,
+    robots: {
+      index: indexable,
+      follow: indexable,
+      googleBot: {
+        index: indexable,
+        follow: indexable,
+      },
+    },
+    openGraph: url
+      ? {
+          images: [
+            {
+              url,
+              width,
+              height,
+              alt,
+            },
+          ],
+        }
+      : null,
+  }
+}
+
+export default async function ProductPage({ params }: PageProps) {
+  const product = await getProduct(params.handle)
+
+  if (!product) return notFound()
+
+  const { priceRange, featuredImage } = product
+
+  const productJsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Product',
+    name: product.title,
+    description: product.description,
+    image: featuredImage.url,
+    offers: {
+      '@type': 'AggregateOffer',
+      availability: product.availableForSale
+        ? 'https://schema.org/InStock'
+        : 'https://schema.org/OutOfStock',
+      priceCurrency: priceRange.minVariantPrice.currencyCode,
+      highPrice: priceRange.maxVariantPrice.amount,
+      lowPrice: priceRange.minVariantPrice.amount,
+    },
+  }
+
+  return (
+    <div className='grid grid-flow-row gap-4 grid-cols-1 sm:grid-cols-2'>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(productJsonLd),
+        }}
+      />
+      <div className='flex justify-center'>
+        <Image
+          alt={product.title}
+          src={featuredImage.url}
+          width={featuredImage.width}
+          height={featuredImage.height}
+          className='mb-3'
+        />
+      </div>
+      <div>
+        <h1 className='text-4xl lg:text-6xl mb-8'>{product.title}</h1>
+        <p className='text-dark-magenta'>{`${new Intl.NumberFormat(undefined, {
+          style: 'currency',
+          currency: priceRange.maxVariantPrice.currencyCode,
+          currencyDisplay: 'narrowSymbol',
+        }).format(parseFloat(priceRange.maxVariantPrice.amount))}`}</p>
+        <p className='mt-5 mb-[15px]'>{product.description}</p>
+      </div>
+    </div>
+  )
+}
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,38 +1,48 @@
 import Image from 'next/image'
 import type { Metadata } from 'next'
 import Link from 'next/link'
+import { getProducts } from '@/lib/shopify'
+import { defaultSort } from '@/lib/constants'
+import { Product } from '@/lib/shopify/types'
 
 export const metadata: Metadata = {
   title: "Shop | Vintage for Y'all",
   description: '',
 }
 
-function ProductCard({ product }: any) {
+function ProductCard({ product }: { product: Product }) {
+  const { title, handle, featuredImage, priceRange } = product
   return (
     <li className='aspect-square'>
       <Link
         prefetch={true}
         className='flex flex-col items-center justify-between'
-        href={`/shop/${product.id}`}
+        href={`/shop/${handle}`}
       >
-        <Image alt={product.title} src={product.image} width={353} height={353} className='mb-3' />
-        <p className='mt-5 mb-[15px]'>{product.title}</p>
-        <p>${product.price.toFixed(2)}</p>
+        <Image alt={title} src={featuredImage?.url} width={353} height={353} className='mb-3' />
+        <p className='mt-5 mb-[15px]'>{title}</p>
+        <p>{`${new Intl.NumberFormat(undefined, {
+          style: 'currency',
+          currency: 'USD',
+          currencyDisplay: 'narrowSymbol',
+        }).format(parseFloat(priceRange.maxVariantPrice.amount))}`}</p>
       </Link>
     </li>
   )
 }
 
 export default async function Shop() {
-  const products = await fetch('https://fakestoreapi.com/products').then((res) => res.json())
+  const { sortKey, reverse } = defaultSort
+  const products = await getProducts({ sortKey, reverse })
+  // console.log(products)
 
   return (
     <>
       <h1 className='text-6xl lg:text-9xl mb-8'>Shop</h1>
       <div>
         <ul className='grid grid-flow-row gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
-          {products.map((product: any) => (
-            <ProductCard key={product.id} product={product} />
+          {products.map((product: Product) => (
+            <ProductCard key={product.handle} product={product} />
           ))}
         </ul>
       </div>
