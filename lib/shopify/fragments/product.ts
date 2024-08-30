87c738774ea5bc24f61be1058f4c4009f6e2e2d3
Joseph Khawly <joseph.khawly@gmail.com>
2024-08-30 13:43:06 -0600

    refactor: remove unused items in product fragment

--- lib/shopify/fragments/product.ts
+++ lib/shopify/fragments/product.ts
@@ -5,15 +5,9 @@ const productFragment = /* GraphQL */ `
   fragment product on Product {
     id
     handle
-    availableForSale
     title
     description
     descriptionHtml
-    options {
-      id
-      name
-      values
-    }
     priceRange {
       maxVariantPrice {
         amount
@@ -22,23 +16,6 @@ const productFragment = /* GraphQL */ `
       minVariantPrice {
         amount
         currencyCode
-      }
-    }
-    variants(first: 250) {
-      edges {
-        node {
-          id
-          title
-          availableForSale
-          selectedOptions {
-            name
-            value
-          }
-          price {
-            amount
-            currencyCode
-          }
-        }
       }
     }
     featuredImage {
