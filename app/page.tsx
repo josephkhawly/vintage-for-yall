53d9772584eb4eb11d81c6275ef0b24fcee7d75d
Joseph Khawly <joseph.khawly@gmail.com>
2024-09-23 19:24:18 -0600

    feat: add new arrivals section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { getProducts } from '@/lib/shopify'
+import { defaultSort } from '@/lib/constants'
+import { Product } from '@/lib/shopify/types'
+import ProductCard from '@/components/ProductCard'
+
+export default async function Home() {
+  const { sortKey, reverse } = defaultSort
+  const products = await getProducts({ sortKey, reverse })
+
   return (
     <>
-      <div className='lg:flex items-start gap-8'></div>
+      <h1 className='text-4xl sm:text-6xl mb-8'>New Arrivals</h1>
+      <div>
+        <ul className='grid grid-flow-row gap-4 md:gap-10 grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+          {products.map((product: Product) => (
+            <ProductCard key={product.handle} product={product} />
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
