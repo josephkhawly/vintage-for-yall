7ee90066191d1b67936f12adcb8815cda2a71a16
Joseph Khawly <joseph.khawly@gmail.com>
2024-09-23 19:37:04 -0600

    fix: shrink header logo on mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,9 @@ import CartModal from './cart/CartModal'
 export default async function Header() {
   const menu = await getMenu('main-menu')
   return (
-    <header className='container mx-auto p-6 md:p-12 flex items-center justify-between'>
+    <header className='container mx-auto p-5 md:p-12 flex items-center justify-between'>
       <Link href='/' prefetch={true}>
-        <Image src={logo1} alt='logo' width={180} />
+        <Image src={logo1} alt='logo' className='w-28 md:w-[180px]' />
       </Link>
       <nav>
         <ul className='flex gap-4 items-center'>
