4677fe6f434fca268a283631d34f786cd5cacea0
Joseph Khawly <joseph.khawly@gmail.com>
2024-09-25 19:23:24 -0600

    feat: image overlay for sold out items

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,21 +4,28 @@ import Price from './Price'
 import { Product } from '@/lib/shopify/types'
 
 export default function ProductCard({ product }: { product: Product }) {
-  const { title, handle, featuredImage, priceRange } = product
+  const { title, handle, featuredImage, priceRange, availableForSale } = product
   return (
-    <li className='aspect-square'>
+    <li>
       <Link
         prefetch={true}
         className='flex flex-col items-center justify-between'
         href={`/shop/${handle}`}
       >
-        <Image
-          alt={title}
-          src={featuredImage?.url}
-          width={353}
-          height={353}
-          className='mb-4 w-full'
-        />
+        <div className='relative'>
+          <Image
+            alt={title}
+            src={featuredImage?.url}
+            width={353}
+            height={353}
+            className='mb-4 w-full aspect-auto'
+          />
+          {!availableForSale && (
+            <div className='absolute top-0 right-0 bg-white bg-opacity-50 text-black text-lg p-1 w-full h-full flex items-center justify-center'>
+              Sold Out
+            </div>
+          )}
+        </div>
         <p className='mb-2 text-center'>{title}</p>
         <Price
           amount={priceRange?.minVariantPrice.amount}
