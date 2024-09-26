c7860de98e07857eca6e36d454e791c2d022e2da
Joseph Khawly <joseph.khawly@gmail.com>
2024-09-26 07:52:43 -0600

    feat: add Substack link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import { FaInstagram } from 'react-icons/fa'
+import { BsSubstack } from 'react-icons/bs'
 
 export default function Footer() {
   return (
@@ -6,6 +7,9 @@ export default function Footer() {
       <div className='flex items-center gap-5'>
         <a href='https://www.instagram.com/vintageforyall' target='_blank'>
           <FaInstagram className='text-3xl text-dark-magenta' />
+        </a>
+        <a href='https://vintageforyall.substack.com' target='_blank'>
+          <BsSubstack className='text-3xl text-dark-magenta' />
         </a>
       </div>
       {/* <p className='text-sm text-center'>
