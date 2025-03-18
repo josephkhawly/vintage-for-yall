4bff172eb03c9b2358c266294928ca77451b996d
Joseph Khawly <joseph.khawly@gmail.com>
2025-03-18 16:30:59 -0600

    feat: add announcement banner to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,26 +16,37 @@ const hardcodedMenu = [
   },
 ]
 
+function AnnouncementBanner() {
+  return (
+    <div className='w-full bg-bubblegum-pink text-white py-2 text-center text-sm'>
+      Come see us at the Silverlake Flea on March 30 and April 5!
+    </div>
+  )
+}
+
 export default async function Header() {
   const menu = await getMenu('main-menu')
   return (
-    <header className='container mx-auto p-5 md:p-12 flex items-center justify-between'>
-      <Link href='/' prefetch={true}>
-        <Image src={logo1} alt='logo' className='w-28 md:w-[180px]' />
-      </Link>
-      <nav>
-        <ul className='flex gap-4 items-center'>
-          {hardcodedMenu.map((item) => (
-            <NavItem key={item.title} item={item} />
-          ))}
-          {menu.map((item) => (
-            <NavItem key={item.title} item={item} />
-          ))}
-          <li className='ml-8'>
-            <CartModal />
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <AnnouncementBanner />
+      <header className='container mx-auto p-5 md:p-12 flex items-center justify-between'>
+        <Link href='/' prefetch={true}>
+          <Image src={logo1} alt='logo' className='w-28 md:w-[180px]' />
+        </Link>
+        <nav>
+          <ul className='flex gap-4 items-center'>
+            {hardcodedMenu.map((item) => (
+              <NavItem key={item.title} item={item} />
+            ))}
+            {menu.map((item) => (
+              <NavItem key={item.title} item={item} />
+            ))}
+            <li className='ml-8'>
+              <CartModal />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
