e496475fc9de14b6bfdf9f2028ae95c627a29fc1
Joseph Khawly <joseph.khawly@gmail.com>
2026-09-18 15:56:33 -0600

    style(homepage): update font weights and italicize text for emphasis

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <div className='flex min-h-[70vh] items-center justify-center'>
       <article className='w-full max-w-xl text-left font-body text-lg leading-relaxed md:text-xl md:leading-relaxed'>
-        <p className='mb-6'>To my vintage-loving baddies,</p>
+        <p className='mb-6 italic'>To my vintage-loving baddies,</p>
 
-        <p className='mb-4'>
-          Our first drop goes live October 2 at 12:00PM PST.
+        <p className='mb-4 italic'>
+          Our first drop goes live <span className='font-bold'>October 2 at 12:00PM PST</span>.
           <br />
-          Sign up below to get first dibs on this &amp; future pieces plus an exclusive
+          Sign up below to get first dibs on this and future pieces, plus an exclusive
           welcome discount.
         </p>
 
@@ -17,8 +17,8 @@ export default function Home() {
           <NewsletterSignup />
         </div>
 
-        <p className='mb-6'>
-          In the meantime, you can buy from us on{' '}
+        <p className='mb-6 italic'>
+          In the meantime, you can still buy from us on{' '}
           <a
             href='https://www.depop.com/uglycryvintage/'
             target='_blank'
@@ -39,15 +39,15 @@ export default function Home() {
           .
         </p>
 
-        <p className='mb-6'>Can&apos;t wait to share more with you!</p>
+        <p className='mb-6 italic'>Can&apos;t wait to share more with you!</p>
 
-        <p className='mb-8'>
-          Xo,
+        <p className='mb-8 italic'>
+          XO,
           <br />
           Ariel
         </p>
 
-        <p className='text-base md:text-lg opacity-90'>
+        <p className='text-base md:text-lg opacity-90 italic'>
           P.S. If you want to keep up with fashion world controversies, what to shop
           secondhand right now (with links!), and vintage in pop culture/current trends, you
           should sign up for UCV&apos;s{' '}
